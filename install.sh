d14491fd78015efd00686b8e2167000e85cd8929
eeeterii <marwanalknah017@gmail.com>
2025-05-22 02:49:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 #!/bin/bash
 git clone https://github.com/eeeterii/Eva-source.git
-cd Eva-source
 pip install -r requirements.txt  # إذا كان هناك متطلبات تثبيت
 python main.py  # أو أي أمر آخر لتشغيل المشروع
 
